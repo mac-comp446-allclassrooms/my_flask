08b76d8330f3d1548cbffc6089342566e58f6f8e
Joslenne <joslennepena@gmail.com>
2021-11-10 12:27:43 -0600

    updating app.py

--- my_flask_practice/app.py
+++ my_flask_practice/app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+#initialize the database
+db = SQLAlchemy(app)
+
+#create a db model
+class Friends(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Name %r' % self.id
 
 @app.route("/")
 def index():
